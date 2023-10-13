e347d3e89445dacfaed99127b9fb2047c8ab0602
Kevin Knights <kevinknights29@hotmail.com>
2023-10-13 15:49:11 -0500

    added project page

---
+++ pages/project.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import os
+from pathlib import Path
+
+import streamlit as st
+
+from src.utils import config
+from src.utils import constants
+
+st.set_page_config(
+    page_title=config.config()["app"]["page_title"],
+    page_icon=config.config()["app"]["page_icon"],
+)
+st.title(config.config()["app"]["project"]["title"])
+
+
+def read_markdown_file(markdown_file: str) -> str:
+    return Path(markdown_file).read_text()
+
+
+project_markdown = read_markdown_file(
+    os.path.join(
+        constants.APP_FOLDER,
+        "README.md",
+    ),
+)
+st.markdown(project_markdown, unsafe_allow_html=True)
