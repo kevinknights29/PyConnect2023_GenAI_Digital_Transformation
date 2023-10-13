6ac7bba04e1799e082d59ddb9ebe691cef0c8da3
Kevin Knights <kevinknights29@hotmail.com>
2023-10-13 00:48:28 -0500

    WIP: adding streaming capabilities

--- src/llm/text_generation.py
+++ src/llm/text_generation.py
@@ -4,6 +4,7 @@ import os
 
 from dotenv import find_dotenv
 from dotenv import load_dotenv
+from langchain.callbacks.streaming_stdout import StreamingStdOutCallbackHandler
 from langchain.chains import LLMChain
 from langchain.llms import OpenAI
 from langchain.prompts import PromptTemplate
@@ -13,26 +14,30 @@ _ = load_dotenv(find_dotenv())
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
 TEMPLATE = (
     "Compórtate como un asistente de eventos.\n"
-    "Tu objetivo es garantizar una buena experiencia para los asistentes.\n"
-    "Tus respuestas deben incluir sesiones de interés para los asistentes, información de valor sobre las sesiones.\n"
+    "Tu objetivo es garantizar una buena experiencia para los participantes del evento.\n"
+    "Tus respuestas deben incluir sesiones de interés e información de valor para los participantes\n"
     "Antes de realizar una acción, pregunta si tienes alguna duda.\n"
     "No compartas información falsa.\n"
     "\n"
-    "Asistente: {question}\n"
-    "Respuesta: "
+    "{history}\n"
+    "Participante: {question}\n"
+    "AI: "
 )
-PROMPT_TEMPLATE = PromptTemplate(template=TEMPLATE, input_variables=["question"])
+PROMPT_TEMPLATE = PromptTemplate(
+    template=TEMPLATE,
+    input_variables=["history", "question"],
+)
 
 
 def _llm_init(**kwargs):
     model_params = {}
     model_params["temperature"] = kwargs.get("temperature", 0.1)
-    model_params["max_length"] = kwargs.get("max_length", 2000)
     model_params["top_p"] = kwargs.get("top_p", 0.95)
-    model_params["top_k"] = kwargs.get("top_p", 40)
 
     llm = OpenAI(
         openai_api_key=OPENAI_API_KEY,
+        streaming=True,
+        callbacks=[StreamingStdOutCallbackHandler()],
         **model_params,
     )
     return llm
